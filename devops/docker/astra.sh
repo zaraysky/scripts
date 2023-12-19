ce016d7c314ebcea9694c8fcf3f4d4c0e5c607b0
Alexander <a.zaraysky@corp.mail.ru>
2023-12-19 09:56:56 +0300

    Add docker on astra

---
+++ devops/docker/astra.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Install Docker & Docker Compose on Astra Linux
+# Based on https://wiki.astralinux.ru/pages/viewpage.action?pageId=158601444
+
+sudo apt update
+sudo apt install docker.io -y
